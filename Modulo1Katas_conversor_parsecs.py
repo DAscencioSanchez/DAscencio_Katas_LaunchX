448234f123040b1e8cca29da017ba36e9ae9aa77
DAscencioSanchez <diaspm77@gmail.com>
2022-02-21 11:42:04 -0600

    Katas terminadas

---
+++ Modulo1Katas_conversor_parsecs.py
@@ -0,0 +1,5 @@
+#Conversor de parsecs
+parsec = input("Numero de parsecs= ")
+lightyears = int(parsec)*3.2616
+
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
---
+++ Modulo1Katas_daytime.py
@@ -0,0 +1,4 @@
+#Mi primer programa 
+from datetime import date 
+
+print("Today's date: " + str(date.today()))
